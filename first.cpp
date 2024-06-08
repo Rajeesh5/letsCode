efc6a665de47ebef6b3ba5ead1d997a16b55f87e
Rajeesh5 <rajeev.bit09@gmail.com>
2024-06-08 21:57:39 +0530

    push from unix

--- first.cpp
+++ first.cpp
@@ -1,30 +1,43 @@
 // C++ Program to calculate the sum of first N natural
 // numbers using recursion
 #include <iostream>
-#include <thread>
+#include <math.h>
+#include <stdio.h>
+// #include <thread>
+
 using namespace std;
   
 int nSum(int n)
 {
     // base condition to terminate the recursion when N = 0
     if (n == 0) {
-        return 0;
+        return 1;
     }  
     // recursive case / recursive call
-    int res = n + nSum(n - 1);  
+    int res = n * nSum(n - 1);  
     return res;
 }
+int fibonacci(int n) {
+    if (n <= 1)
+        return n;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
   
 int main()
 {
     int n = 5;  
     // calling the function
     int sum = nSum(n);  
-    cout << "Sum = " << sum;
+    cout << "Sum = " << sum << std::endl;
 
-    while(1){
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    }
+    printf("%d\n",fibonacci(6));
+    cout << fibonacci(6) << std::endl;
+
+    // while(1){
+    //     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    // }
     return 0;
 }
 
